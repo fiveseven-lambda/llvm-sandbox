4769bee35727e68d49e6e65fa66d0c584987b141
fiveseven-lambda <fiveseven.lambda@gmail.com>
2025-05-09 16:42:02 +0900

    Fixed build.rs (added llvm-config --system-libs)

--- JIT/self-contained-jit/build.rs
+++ JIT/self-contained-jit/build.rs
@@ -2,15 +2,37 @@ use std::process::Command;
 
 fn main() {
     let llvm_config = std::env::var("LLVM_CONFIG").unwrap();
-    let get_config = |arg: &str| Command::new(&llvm_config).arg(arg).output().unwrap().stdout;
-    let cxxflags = get_config("--cxxflags");
-    let cxxflags = std::str::from_utf8(&cxxflags).unwrap();
-    let cxxflags = shlex::split(cxxflags).unwrap();
-    let libdir = get_config("--libdir");
-    let libdir = std::str::from_utf8(&libdir).unwrap();
-    let libs = get_config("--libs");
-    let libs = std::str::from_utf8(&libs).unwrap();
-    let libs = shlex::split(libs).unwrap();
+    let cxxflags = shlex::split(
+        std::str::from_utf8(
+            &Command::new(&llvm_config)
+                .arg("--cxxflags")
+                .output()
+                .unwrap()
+                .stdout,
+        )
+        .unwrap(),
+    )
+    .unwrap();
+    let libdir = String::from_utf8(
+        Command::new(&llvm_config)
+            .arg("--libdir")
+            .output()
+            .unwrap()
+            .stdout,
+    )
+    .unwrap();
+    let libs = shlex::split(
+        std::str::from_utf8(
+            &Command::new(&llvm_config)
+                .arg("--libs")
+                .arg("--system-libs")
+                .output()
+                .unwrap()
+                .stdout,
+        )
+        .unwrap(),
+    )
+    .unwrap();
 
     println!("cargo::rerun-if-changed=src/backend.cpp");
     let mut build = cc::Build::new();
@@ -22,11 +44,9 @@ fn main() {
     }
     build.compile("backend");
 
-    println!("cargo:rustc-link-search={}", libdir);
+    println!("cargo::rustc-link-search={}", libdir);
     for lib in &libs {
-        println!("cargo:rustc-link-lib={}", lib.strip_prefix("-l").unwrap());
+        println!("cargo::rustc-link-lib={}", lib.strip_prefix("-l").unwrap());
     }
-    println!("cargo:rustc-link-lib=z");
-    println!("cargo:rustc-link-lib=zstd");
     println!("cargo::rustc-link-arg=-export-dynamic");
 }
